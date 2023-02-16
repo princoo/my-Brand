dbf7d766f17ace434f9fee39d70e29d2f8e28730
princoo <princeineza@gmail.com>
2023-02-17 00:13:55 +0200

    adding access headers

--- NODE_MONGO_TUTORIAL/src/controllers/blogcontrollers.js
+++ NODE_MONGO_TUTORIAL/src/controllers/blogcontrollers.js
@@ -255,7 +255,7 @@ const deleteMessage= async(req,res)=>{
 const viewmessages= async(req,res)=>{
     await Message.find()
         .then((data)=> {
-            res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+            res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/UI/html/winmessages.html');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
             res.status(200).json({"message":data})})
         // .then((data)=>{
