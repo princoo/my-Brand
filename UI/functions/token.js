341d393ea2a613966b0e80c7ea90de66ff28ef75
princoo <princeineza@gmail.com>
2023-02-24 15:36:13 +0200

    view dashboard in nav

--- UI/functions/token.js
+++ UI/functions/token.js
@@ -5,3 +5,17 @@ var cookies= document.cookie
 ({ ...accumulator,[key.trim()]: decodeURIComponent(value)}),{})
 
 // module.exports= cookies
+  async function viewAdmin() {
+    const res= await fetch(`https://my-brand-production-b3f0.up.railway.app/admin`, 
+    {
+     method: 'GET',
+     headers: {
+             'Authentication': `Bearer ${cookies.jwt}`,
+             'Content-Type': 'application/json',
+        },
+     }); 
+     const data= await res.json()
+     return data
+ }
+
+
